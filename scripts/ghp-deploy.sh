206d655a905d4490b4dcd2f38b5890dfc8cb4e83
Fernando Crespo <fernando82@gmail.com>
2019-09-06 00:04:52 -0300

    GitHub Pages Deploy script

---
+++ scripts/ghp-deploy.sh
@@ -0,0 +1,13 @@
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+CNAME=`cat $SCRIPTPATH/../src/CNAME`
+ng build --prod --baseHref "http://$CNAME"
+
+OUT=$? #exit code from last command
+if [ $OUT -eq 0 ] ; then
+    read -p 'Deploy? [y|N] ' deploy
+    case $deploy in
+        y|Y) echo "Deploying";
+        $SCRIPTPATH/..//node_modules/angular-cli-ghpages/bin/angular-cli-ghpages;;
+        *) echo "Not deploying";;
+    esac
+fi
